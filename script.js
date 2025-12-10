f22c79a594985cbef81fe4d61bb90bbf746527fe
baouzjulien82 <baouzjulien@gmail.com>
2025-12-10 12:04:56 +0100

    Mise à jour generale

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  // Sélectionne tous les liens sauf le dropdown-toggle
+  document.querySelectorAll('.navbar-nav .nav-link:not(.dropdown-toggle)')
+    .forEach(function(navLink) {
+      navLink.addEventListener('click', function () {
+        var navbarCollapse = document.querySelector('.navbar-collapse');
+        if (navbarCollapse.classList.contains('show')) {
+          var bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+            toggle: true
+          });
+        }
+      });
+    });
+});
